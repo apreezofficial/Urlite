1dcf5d862b5b76bd158a88ffa4bfd9dd6b82a501
apreezofficial <apreezofficial@gmail.com>
2025-03-20 17:15:25 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from "./components/Nav"
 import Page from "./components/Page"
+import Footer from "./components/footer"
 
 const App = () => {
   return (
       <>
         <Nav />
         <Page />
+        <Footer />
       </>
     )
 }
