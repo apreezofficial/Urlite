3a905ac6789e974ede1cd2fe7c839c89d0deab5f
apreezofficial <apreezofficial@gmail.com>
2025-03-20 13:11:24 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav"
+import MainWithHero from "./components/MainWithHero"
+
+const App = () => {
+  return (
+      <>
+        <Nav />
+        <MainWithHero />
+      </>
+    )
+}
+export default App;
