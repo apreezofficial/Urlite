cd8bb2d5c5e5646cd3e93a27e60ce54fb734585d
Precious Adedokun <apreezofficial@gmail.com>
2025-03-21 18:35:56 +0100

    Update Page.jsx

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -87,9 +87,7 @@ const Page = () => {
     };
 
     return (
-        <div>
-            <main className="h-[40rem] w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div><main className="min-h-screen w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center"> <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
                 <div className="flex text-center flex-col gap-[1.75rem] w-[315px] md:w-[500px] lg:w-[680px] -mt-[4.8rem] sm:-mt-[2.5rem]">
                     <motion.h1
                         initial="hidden"
